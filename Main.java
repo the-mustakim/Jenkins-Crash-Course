fdb65226121d4bef59a439c0b893538ae9726f8b
Mustakim <mustakimsayyed50@gmail.com>
2025-10-27 22:40:30 +0000

    add sample test case

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ public class Main{
         System.out.println("This is java program");
         System.out.println("Output from java program");
         System.out.println(new Date());
+        System.out.println("This is another line for checking the build...");
     }
 }
